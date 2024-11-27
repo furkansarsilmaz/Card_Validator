358c5d2afbc1aafdcdafe19d55b04deaec371caf
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2024-11-27 23:30:21 +0300

    added functions for luhn

--- card.py
+++ card.py
@@ -6,20 +6,29 @@ class Card :
         """
         self.root = root 
         self.root.geometry("300x200")
+        self.root.configure(background = "gray")
         self.root.Label_1 = Label(self.root,text="Welcome\nEnter your card :",font=("Arial",12))
-        self.root.Label_1.pack()
+        self.root.Label_1.pack(pady=10)
 
         self.root.Text_1 = Text(self.root,width=11,height=1,relief=GROOVE)
-        self.root.Text_1.pack()
+        self.root.Text_1.pack(pady=10)
 
         self.root.Button_1 = Button(self.root,text="Submit",width=6,height=2,command= self.getCardID)
-        self.root.Button_1.pack()
+        self.root.Button_1.pack(pady=10)
     
     def getCardID (self):
-        nums = self.root.Text_1.get("1.0",END)
+        nums = self.root.Text_1.get("1.0",END).strip()
         print(nums)
+        self.getEvenNum(nums)
+        self.getOddNum(nums)
+    
+    def getEvenNum(self,nums):
+        for i in range(len(nums)-2, -1, -2):
+            print(nums[i]) 
 
-
+    def getOddNum(self,nums):
+        for i in range(len(nums)-1,-1,-2):
+            print(nums[i])
 
 if __name__ == "__main__":
     root = Tk ()
